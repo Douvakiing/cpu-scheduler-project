1fb0b93e41473f8cf5bc7887affeee1e0e47082f
Omar Wael <dragontooth304@gmail.com>
2026-04-14 01:54:56 +0200

    Added functions for waiting time, turnaround time, and average for both in scheduer.h

--- src/Scheduler.h
+++ src/Scheduler.h
@@ -24,6 +24,11 @@ public:
     void RR(int timeQuantum);
     void Priority_Premetive();
     void Priority_NonPremetive();
+
+    int calculateWaitingTime(); // Per Process
+    int calculateTurnAroundTime(); // Per Process
+    int calculateAvgWait(); // For all processes
+    int calculateAvgTurnAround(); // For all processes
 };
 
 #endif
